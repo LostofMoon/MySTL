c4f41e2685a4e1d216d318c7208e87812675243f
LostOfMoon <1833775406@qq.com>
2023-09-13 14:49:54 +0800

    most

--- MyList.hpp
+++ MyList.hpp
@@ -8,42 +8,55 @@
 #include <cstddef>
 
 namespace sjtu {
-/**
- * a data container like std::list
- * allocate random memory addresses for data and they are doubly-linked in a list.
- */
+
 template<typename T>
 class list {
 public:
     // types
     using value_type = T;
     using size_type = unsigned long long;
+    //statement 
+    class iterator;
+    class const_iterator;
 
 protected:
     class node {
     public:
         node *pre, *next;
         value_type *data;
+        list *ID;
 
-        node():pre(nullptr),next(nullptr),data(nullptr){
+        node():pre(nullptr), next(nullptr), data(nullptr), ID(nullptr) {
         }
 
-        node(value_type x, node* p = nullptr,  node* q = nullptr):data(new value_type(x) ),pre(p),next(q){
+        node(value_type x, node* p = nullptr,  node* q = nullptr):data(new value_type(x) ),pre(p),next(q), ID(nullptr) {
         }
 
+        node &operator=(const node &other) {
+            pre = other.pre;
+            next = other.next;
+            data = other.data;
+            ID = other.ID;
+        }
+
+        ~node() {
+            delete data;
+        }
     };
 
 protected:
-    node* head, tail;
+    node *head, *tail;
     size_type currentsize;
 
     node *insert(node *pos, node *cur) {
-        node* pre = cur->pre;
+        cur->ID = pos->ID;
+        node* pre = pos->pre;
 
-        pos->pre = pre;
-        pre->next = pos;
-        pos->next = cur;
-        cur->pre = pos;
+        cur->pre = pre;
+        pre->next = cur;
+
+        cur->next = pos;
+        pos->pre = cur;
 
         return cur;
     }
@@ -58,11 +71,9 @@ protected:
     }
 
 public:
-    //statement
-    class const_iterator;
-
 //-------------------------------iterator------------------------------------------
     class iterator {
+        friend list;
         friend list::const_iterator;
     private:
         node* data_;
@@ -71,13 +82,13 @@ public:
         iterator():data_(nullptr) {
         }
         
-        iterator(const value_type* x):data_(x) {
+        iterator(node* x):data_(x) {
         }
 
         iterator(const iterator& x):data_(x.data_) {
         }
 
-        iterator(const const_iterator& x):data_(x.data_) {
+        iterator(const_iterator& x):data_(x.data_) {
         }
 
         iterator(iterator&& x):data_(x.data_) {
@@ -88,14 +99,14 @@ public:
         iterator & operator++() {
             if(data_ == nullptr) throw invalid_iterator();
             data_ = data_->next;
-            if(data_ == nullptr) throw invalid_iterator();
+            if(data_->next == nullptr) throw invalid_iterator();
             return *this;
         }
         
         iterator & operator--() {
             if(data_ == nullptr) throw invalid_iterator();
             data_ = data_->pre;
-            if(data_ == nullptr) throw invalid_iterator();
+            if(data_->pre == nullptr) throw invalid_iterator();
             return *this;
         }
 
@@ -103,7 +114,7 @@ public:
             iterator temp = *this;
             if(data_ == nullptr) throw invalid_iterator();
             data_ = data_->next;
-            if(data_ == nullptr) throw invalid_iterator();
+            if(data_->next == nullptr) throw invalid_iterator();
             return temp;
         }
         
@@ -111,51 +122,52 @@ public:
             iterator temp = *this;
             if(data_ == nullptr) throw invalid_iterator();
             data_ = data_->pre;
-            if(data_ == nullptr) throw invalid_iterator();
+            if(data_->pre == nullptr) throw invalid_iterator();
             return temp;
         }
 
         value_type & operator *() const {
-            if(data_ == nullptr) throw invalid_iterator();
-            return *data_;
+            if(data_->data == nullptr) throw invalid_iterator();
+            return *(data_->data);
         }
         
         value_type * operator ->() const noexcept {
-            if(data_ == nullptr) throw invalid_iterator();
-            return data_;
+            if(data_->data == nullptr) throw invalid_iterator();
+            return data_->data;
         }
 
         bool operator==(const iterator &rhs) const {
-            return data_ == rhs->data_;
+            return data_ == rhs.data_;
         }
         bool operator==(const const_iterator &rhs) const {
-            return data_ == rhs->data_;
+            return data_ == rhs.data_;
         }
 
         bool operator!=(const iterator &rhs) const {
-            return data_ != rhs->data_;
+            return data_ != rhs.data_;
         }
 
         bool operator!=(const const_iterator &rhs) const {
-            return data_ != rhs->data_;
+            return data_ != rhs.data_;
         }
     };
 
 //-------------------------------const_iterator------------------------------------------
 
     class const_iterator {
-    friend list::iterator;
+        friend list;
+        friend list::iterator;
     private:
         node const * data_;
     public:
         //build and destroy
-        const_iterator(const value_type const * x):data_(x) {
+        const_iterator(const node* x):data_(x) {
         }
 
-        const_iterator(const iterator const & x):data_(x.data_) {
+        const_iterator(const iterator & x):data_(x.data_) {
         }
 
-        const_iterator(const const_iterator & x):data_(x.data_) {
+        const_iterator(const_iterator & x):data_(x.data_) {
         }
 
         const_iterator(iterator&& x):data_(x.data_) {
@@ -171,7 +183,7 @@ public:
         }
         
         const_iterator & operator--() {
-            if(tdata_ == nullptr) throw invalid_iterator();
+            if(data_ == nullptr) throw invalid_iterator();
             data_ = data_->pre;
             if(data_ == nullptr) throw invalid_iterator();
             return *this;
@@ -195,27 +207,27 @@ public:
 
         value_type & operator *() const {
             if(data_ == nullptr) throw invalid_iterator();
-            return *data_;
+            return *(data_->data);
         }
         
         value_type * operator ->() const noexcept {
             if(data_ == nullptr) throw invalid_iterator();
-            return data_;
+            return data_->data;
         }
 
         bool operator==(const iterator &rhs) const {
-            return data_ == rhs->data_;
+            return data_ == rhs.data_;
         }
         bool operator==(const const_iterator &rhs) const {
-            return data_ == rhs->data_;
+            return data_ == rhs.data_;
         }
 
         bool operator!=(const iterator &rhs) const {
-            return data_ != rhs->data_;
+            return data_ != rhs.data_;
         }
 
         bool operator!=(const const_iterator &rhs) const {
-            return data_ != rhs->data_;
+            return data_ != rhs.data_;
         }
     };
 
@@ -223,16 +235,18 @@ public:
 
     //build and destroy
     list() {
-        head = new node;
-        tail = new node;
+        head = new node();
+        tail = new node();
         head->next = tail;
         tail->pre = head;
+        head->ID = this;
+        tail->ID = this;
         currentsize = 0;
     }
 
     list(const list &other):list() {
         node* add = other.head->next;
-        while(add != other->tail){
+        while(add != other.tail){
             insert(tail, new node(*(add->data)) );
             add = add->next;
         }
@@ -254,41 +268,46 @@ public:
         tail = new node;
         head->next = tail;
         tail->pre = head;
+        head->ID = this;
+        tail->ID = this;
 
         node* add = other.head->next;
-        while(add != other->tail){
+        while(add != other.tail){
             insert(tail, new node(*(add->data)) );
             add = add->next;
         }
         currentsize = other.currentsize;
+        return *this;
     }
     
     const value_type & front() const {
         if(empty())throw container_is_empty();
-        return head->next->data;
+        return *(head->next->data);
     }
 
     const value_type & back() const {
         if(empty())throw container_is_empty();
-        return tail->pre->data;
+        return *(tail->pre->data);
     }
-     
+    
+    //iterator
     iterator begin() {
         return iterator(head->next);
     }
 
     const_iterator cbegin() const {
-        return const_terator(head->next);
+        return const_iterator(head->next);
     }
 
     iterator end() {
-        return const_terator(tail);
+        return iterator(tail);
     }
 
     const_iterator cend() const {
-        return const_terator(tail);
+        return const_iterator(tail);
     }
     
+    //function
     virtual bool empty() const {
         return (currentsize == 0);
     }
@@ -314,51 +333,73 @@ public:
     
     void push_back(const value_type &value) {
         insert(tail, new node(value) );
+        currentsize++;
     }
 
     void push_front(const T &value) {
         insert(head->next, new node(value) );
+        currentsize++;
     }
 
     void pop_back() {
+        if(currentsize == 0)throw container_is_empty();
         erase(tail->pre);
+        currentsize--;
     }
     
     void pop_front() {
+        if(currentsize == 0)throw container_is_empty();
         erase(head->next);
+        currentsize--;
     }
 
     virtual iterator insert(iterator pos, const T &value) {
-        insert(pos, new node(value) );
+        if(pos.data_->ID != this) throw invalid_iterator();
+        if(pos.data_ == nullptr) throw index_out_of_bound();
+        currentsize++;
+        return insert(pos.data_, new node(value) );
     }
 
     virtual iterator erase(iterator pos) {
-        erase(pos.data_);
+        if(pos.data_ == nullptr) throw index_out_of_bound();
+        if(pos.data_->data == nullptr) throw index_out_of_bound();
+        currentsize--;
+        return erase(pos.data_);
     }
     /**
      * sort the values in ascending order with operator< of T
      */
     void sort() {}
-    /**
-     * merge two sorted lists into one (both in ascending order)
-     * compare with operator< of T
-     * container other becomes empty after the operation
-     * for equivalent elements in the two lists, the elements from *this shall always precede the elements from other
-     * the order of equivalent elements of *this and other does not change.
-     * no elements are copied or moved
-     */
-    void merge(list &other) {}
-    /**
-     * reverse the order of the elements
-     * no elements are copied or moved
-     */
-    void reverse() {}
-    /**
-     * remove all consecutive duplicate elements from the container
-     * only the first element in each group of equal elements is left
-     * use operator== of T to compare the elements.
-     */
-    void unique() {}
+
+    void merge(list &other) {
+        node *index = head->next, *pos = other.head->next;
+        while(pos != other.tail){
+            if(index->data == nullptr || *(pos->data) < *(index->data)){
+                insert(index, *(pos->data));
+                pos = pos->next;
+            }
+            else index = index->next;
+        }
+    }
+
+    void reverse() {
+        list temp;
+
+    }
+    void unique() {
+        if(currentsize < 2) return;
+        node *now = head->next, *then = now->next;
+        while(then != tail){
+            if(*(now->data) == *(then->data)){
+                then = erase(then);
+                currentsize--;
+            }
+            else{
+                then = then->next;
+                now = now->next;
+            }
+        }
+    }
 };
 
 }
